74beb977dbe1cbb8c6aadf85f3e2eed29599af07
alejandro <alejandro.delamoracazalis@assentcompliance.com>
2025-06-18 16:37:05 -0400

    Switch to externalbrowser Snowflake auth

--- src/fetcher.py
+++ src/fetcher.py
@@ -28,11 +28,11 @@ class SnowflakeFetcher:
     def __init__(self):
         self.connection_params = {
             'user': os.getenv('SNOWFLAKE_USER'),
-            'password': os.getenv('SNOWFLAKE_PASSWORD'),
             'account': os.getenv('SNOWFLAKE_ACCOUNT'),
             'warehouse': os.getenv('SNOWFLAKE_WAREHOUSE'),
             'database': os.getenv('SNOWFLAKE_DATABASE'),
-            'schema': os.getenv('SNOWFLAKE_SCHEMA')
+            'schema': os.getenv('SNOWFLAKE_SCHEMA'),
+            'authenticator': os.getenv('SNOWFLAKE_AUTHENTICATOR', 'externalbrowser')
         }
         
         self.logger = logging.getLogger(__name__)
